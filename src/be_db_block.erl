eb3c6faf066eb6f6ab3fd63cc16dbdc853b9ff0d
Anthony Anderson <fnanderson93@gmail.com>
2022-03-24 15:54:04 -0400

    copy try #2

--- src/be_db_block.erl
+++ src/be_db_block.erl
@@ -85,7 +85,9 @@ copy_transactions_list(CopyList, Conn) ->
         Conn,
         CopyList,
         infinity
-    ).
+    )
+    {ok, Count} = epgsql:copy_done(Conn),
+    lager:info("Copy is completed, added ~p rows!", [Count]).
 
 %%
 %% be_block_handler
