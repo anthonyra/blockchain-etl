8abb7a8b3fb347ac62a5b0a21a6952b57f764f9d
Anthony Anderson <fnanderson93@gmail.com>
2022-03-30 09:11:51 -0400

    Only nested list guard

--- src/be_txn.erl
+++ src/be_txn.erl
@@ -13,7 +13,7 @@ append([], L) -> L.
 
 flatten_once(List) ->
     flatten_once(List, []).
-flatten_once([H|T], L) when is_list(H) ->
+flatten_once([H|T], L) when is_list(lists:last(H)) ->
     flatten_once(T, append(H, L));
 flatten_once([H|T], L) ->
     flatten_once(T, [H | L]);
